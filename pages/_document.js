a88d7bd7c5ffa21bc99606344538017ae98db1da
Mr. Hasan <hasanshahriar32@gmail.com>
2024-12-11 14:57:43 +0600

    Refactor Giscus integration to use dynamic script loading and replace posts index with MDX format

--- pages/_document.js
+++ pages/_document.js
@@ -21,24 +21,33 @@ export default function Document() {
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
-        <script
-          src="https://giscus.app/client.js"
-          data-repo="hasanshahriar32/mealman"
-          data-repo-id="R_kgDONIa5xg"
-          data-category-id="DIC_kwDONIa5xs4ClHoR"
-          data-mapping="pathname"
-          data-strict="0"
-          data-reactions-enabled="1"
-          data-input-position="top"
-          data-theme="light"
-          data-lang="en"
-          crossOrigin="anonymous"
-          async
-        ></script>
       </Head>
       <body>
         <Main />
         <NextScript />
+        <div style={{ maxWidth: '600px', margin: '0 auto', padding: '16px' }} id="giscus-container"></div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                const script = document.createElement('script');
+                script.src = 'https://giscus.app/client.js';
+                script.async = true;
+                script.crossOrigin = 'anonymous';
+                script.setAttribute('data-repo', 'hasanshahriar32/mealman');
+                script.setAttribute('data-repo-id', 'R_kgDONIa5xg');
+                script.setAttribute('data-category-id', 'DIC_kwDONIa5xs4ClHoR');
+                script.setAttribute('data-mapping', 'pathname');
+                script.setAttribute('data-strict', '0');
+                script.setAttribute('data-reactions-enabled', '1');
+                script.setAttribute('data-input-position', 'top');
+                script.setAttribute('data-theme', 'light');
+                script.setAttribute('data-lang', 'en');
+                document.getElementById('giscus-container').appendChild(script);
+              })();
+            `,
+          }}
+        ></script>
       </body>
     </Html>
   );

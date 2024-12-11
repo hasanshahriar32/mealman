0933723cf69555554bde7e2ff96e34cbe2739370
Mr. Hasan <hasanshahriar32@gmail.com>
2024-12-11 14:34:05 +0600

    Refactor document structure to integrate Giscus comments script directly in the Head component for improved performance

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
-  }
+    title: "Next.js Blog Starter Kit",
+    description: "Clone and deploy your own Next.js portfolio in minutes.",
+    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+  };
 
   return (
     <Html lang="en">
@@ -21,26 +21,24 @@ export default function Document() {
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
+        <script
+          src="https://giscus.app/client.js"
+          data-repo="hasanshahriar32/mealman"
+          data-repo-id="R_kgDONIa5xg"
+          data-category-id="DIC_kwDONIa5xs4ClHoR"
+          data-mapping="pathname"
+          data-strict="0"
+          data-reactions-enabled="1"
+          data-input-position="top"
+          data-theme="light"
+          data-lang="en"
+          crossOrigin="anonymous"
+          async
+        ></script>
       </Head>
       <body>
         <Main />
         <NextScript />
-        <Head>
-          <script
-            src="https://giscus.app/client.js"
-            data-repo="hasanshahriar32/mealman"
-            data-repo-id="R_kgDONIa5xg"
-            data-category-id="DIC_kwDONIa5xs4ClHoR"
-            data-mapping="pathname"
-            data-strict="0"
-            data-reactions-enabled="1"
-            data-input-position="top"
-            data-theme="light"
-            data-lang="en"
-            crossOrigin="anonymous"
-            async
-          ></script>
-        </Head>
       </body>
     </Html>
   );

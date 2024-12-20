a01896db20d8a4a8e0583726141d5a683d1d798b
Mr. Hasan <hasanshahriar32@gmail.com>
2024-12-20 17:50:52 +0600

    Add ThemeProvider component and integrate it into the layout for improved theming support

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Manrope } from 'next/font/google';
 import { UserProvider } from '@/lib/auth';
 import { getUser } from '@/lib/db/queries';
 import BackToTop from '@/components/back-to-top';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export const viewport: Viewport = {
   maximumScale: 1,
@@ -66,9 +67,16 @@ export default function RootLayout({
       lang="en"
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
     >
-      <body className="min-h-[100dvh] bg-gray-50">
-        <UserProvider userPromise={userPromise}>{children}</UserProvider>
-        <BackToTop />
+      <body className="min-h-[100dvh] bg-gray-50 dark:bg-gray-950">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <UserProvider userPromise={userPromise}>{children}</UserProvider>
+          <BackToTop />
+        </ThemeProvider>
       </body>
     </html>
   );
